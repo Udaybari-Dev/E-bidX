c9df227e3ca93ab8006c6539f52d490274097191
Udaybari324 <Udaybari17@gmail.com>
2025-05-05 12:53:20 +0530

    config-app.js-in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 
 
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(cors({origin:["http://localhost:5173", "https://e-bid-x.vercel.app"],credentials:true}));
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
