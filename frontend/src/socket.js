795be3b03067b86ae7d6bbfc3d8a97afeaf5ae1d
Udaybari324 <Udaybari17@gmail.com>
2025-05-05 13:42:48 +0530

    config-socket.js-and-index.js-in backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,8 @@
 // src/socket.js
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket = io("https://e-bidx.onrender.com"); 
+const socket = io("https://e-bidx.onrender.com", {
+  withCredentials: true,
+});
 
 export default socket;
